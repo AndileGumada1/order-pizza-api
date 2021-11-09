450d7bb14797f3e5697695d8165053f07dcfedf0
andile_gumada <decemberandile@gmail.com>
2021-11-09 15:24:03 +0200

    added the authentication token class

---
+++ src/main/java/com/andile/houseofpizza/persistence/model/AuthenticationToken.java
@@ -0,0 +1,23 @@
+package com.andile.houseofpizza.persistence.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "tokens")
+public class AuthenticationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    private String token;
+
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+}
